eb3229d01646a1ebb9eee859e87ff8cf5f36c970
Zhang <Yang.Z@Dell.com>
2020-05-27 04:31:29 -0400

    changed types

--- pkg/apis/samplecontroller/v1alpha1/types.go
+++ pkg/apis/samplecontroller/v1alpha1/types.go
@@ -23,32 +23,32 @@ import (
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// Foo is a specification for a Foo resource
-type Foo struct {
+// CapsRoute is a specification for a CapsRoute resource
+type CapsRoute struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   FooSpec   `json:"spec"`
-	Status FooStatus `json:"status"`
+	Spec   CapsRouteSpec   `json:"spec"`
+	Status CapsRouteStatus `json:"status"`
 }
 
-// FooSpec is the spec for a Foo resource
-type FooSpec struct {
-	DeploymentName string `json:"deploymentName"`
-	Replicas       *int32 `json:"replicas"`
+// CapsRouteSpec is the spec for a CapsRoute resource
+type CapsRouteSpec struct {
+	ServiceName string `json:"serviceName"`
+	RouteName   string `json:"routeName"`
 }
 
-// FooStatus is the status for a Foo resource
-type FooStatus struct {
-	AvailableReplicas int32 `json:"availableReplicas"`
+// CapsRouteStatus is the status for a CapsRoute resource
+type CapsRouteStatus struct {
+	FullRouteName string `json:"fullRouteName"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// FooList is a list of Foo resources
-type FooList struct {
+// CapsRouteList is a list of CapsRoute resources
+type CapsRouteList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
 
-	Items []Foo `json:"items"`
+	Items []CapsRoute `json:"items"`
 }
